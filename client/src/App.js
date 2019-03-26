1618186aea278c8f080484a46432c717dadf92ce
Marious <mlancelot88@gmail.com>
2019-03-26 16:55:23 -0500

    Added Auth0 dependency. Settled env's for Auth0 integration. Auth0 form displays upons user click on Login.

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Auth0Lock } from "auth0-lock";
 
 // testing bootstrap
 import Button from 'react-bootstrap/Button';
 
+var lock = new Auth0Lock(
+  process.env.REACT_APP_CLIENT_ID,
+  process.env.REACT_APP_DOMAIN_URL
+);
+
 class App extends Component {
   render() {
+    console.log("PROCESS: ", process.env)
     return (
       <div className="App">
         <header className="App-header">
           <h1>Focus Timer</h1>
           {/* testing button from bootstrap */}
-          <Button variant="primary">Primary</Button>
+          <Button onClick={function() {
+            lock.show()
+            }} variant="primary">Login</Button>
         </header>
       </div>
-    );
-  }
-}
+    )}}
 
 export default App;
