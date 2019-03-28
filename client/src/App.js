d14023de71645e9c8279244f2f42ca13d2a58dca
sean-one <basement365@gmail.com>
2019-03-27 22:44:36 -0700

    slack add button

--- client/src/App.js
+++ client/src/App.js
@@ -41,6 +41,9 @@ class App extends Component {
           <Button onClick={function() {
             lock.show()
             }} variant="primary">Login</Button>
+          <div>
+            <a href="https://slack.com/oauth/authorize?scope=commands,bot&client_id=586899066608.590399489303"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
+          </div>
         </header>
         <div className="name-container">
           <UserListView />
