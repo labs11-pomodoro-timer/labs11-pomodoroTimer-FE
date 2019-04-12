b92a9fbcd047e36bd45ddb18cd09a80d6d5031bd
sean-one <basement365@gmail.com>
2019-04-11 22:19:35 -0700

    added vairable for local host and focustimer addy

--- client/src/App.js
+++ client/src/App.js
@@ -41,8 +41,6 @@ class App extends Component {
     }
   }
 
-
-
   handleClose() {
     this.setState({ modalShow: false });
   }
--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
+//! change between for prod & dev
+// http://localhost:8000
+// https://focustimer-labs11.herokuapp.com
+const hostUrl = 'http://localhost:8000';
+
 export const START_TIME = 'START_TIME';
 export const START_TIME_SUCCESS = 'START_TIME_SUCCESS';
 export const START_TIME_FAILURE = 'START_TIME_FAILURE';
@@ -7,10 +12,9 @@ export const START_TIME_FAILURE = 'START_TIME_FAILURE';
 export const timer = (time) => dispatch => {
   dispatch({ type: START_TIME });
   axios
-    // http://localhost:8000/api/timer/start/${time}
-    // https://focustimer-labs11.herokuapp.com/api/timer/start/${time}
-    .get(`http://localhost:8000/api/timer/start/${time}`)
+    .get(`${hostUrl}/api/timer/start/${time}`)
     .then(response => {
+      console.log('right here', response.data);
       dispatch({ type: START_TIME_SUCCESS, payload: response.data });
     })
     .catch(error => {
@@ -25,9 +29,7 @@ export const GET_TIME_FAILURE = 'GET_TIME_FAILURE';
 export const getTime = (id) => dispatch => {
   dispatch({ type: GET_TIME });
   axios
-    // http://localhost:8000/api/timer/checkTimer/${id}
-    // https://focustimer-labs11.herokuapp.com/api/timer/checkTimer/${id}
-    .get(`http://localhost:8000/api/timer/checkTimer/${id}`)
+    .get(`${hostUrl}/api/timer/checkTimer/${id}`)
     .then(response => {
       dispatch({ type: GET_TIME_SUCCESS, payload: response.data });
     })
@@ -44,8 +46,7 @@ export const FETCH_EMAIL_FAILURE = 'FETCH_EMAIL_FAILURE';
 export const getEmail = (email) => dispatch => {
   dispatch({ type: FETCH_EMAIL });
   axios
-    // .get(`https://focustimer-labs11.herokuapp.com/api/users/${email}`)
-    .get(`http://localhost:8000/api/users/${email}`)
+    .get(`${hostUrl}/api/users/${email}`)
     .then(res => {
       dispatch({ type: FETCH_EMAIL_FOUND, payload: res.data });
       localStorage.setItem('id', res.data[0].id)
