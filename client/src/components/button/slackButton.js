a6ff5f6e79c3f4ad83db7f17aae9ca32f0943316
Steven O'Neill <oneillsteven957@gmail.com>
2019-04-15 12:36:52 -0500

    Fixed modal reload issue and added permissions to slack button

--- client/src/components/button/slackButton.js
+++ client/src/components/button/slackButton.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SlackButton = () => {
     return (
         <div>
-            <a href={`https://slack.com/oauth/authorize?client_id=586899066608.590399489303&scope=bot,commands,dnd:write,dnd:read,channels:write,groups:write,mpim:write,im:write&state=${localStorage.getItem('email')}`}><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
+            <a href={`https://slack.com/oauth/authorize?client_id=586899066608.590399489303&scope=bot,commands,users.profile:write,dnd:write,dnd:read,channels:write,groups:write,mpim:write,im:write&state=${localStorage.getItem('email')}`}><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
         </div>
     );
 }
